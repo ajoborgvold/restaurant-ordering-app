bf5a77449d879ecd224f79ca6a23acc317b3ab92
ajo <115482909+ajoborgvold@users.noreply.github.com>
2024-01-16 11:31:53 +0100

    MenuCard layout; added button

--- src/components/MenuCard.tsx
+++ src/components/MenuCard.tsx
@@ -5,12 +5,19 @@ export default function MenuCard(): JSX.Element {
     return (
       <div
         key={index}
-        className="w-full grid grid-cols-2 items-center border-b border-slate-50 p-2"
+        className="w-full flex gap-6 items-center border-b border-slate-50 last:border-none py-8"
       >
-        <h3 className="capitalize font-semibold tracking-wide">{item.name}</h3>
-        <p>{item.ingredients.join(', ')}</p>
-        <p className="font-bold">${item.price}</p>
-        <div className="order-first row-span-2 flex items-center text-7xl">
+        <div className="flex flex-col gap-1">
+          <h3 className="capitalize font-semibold tracking-wide">
+            {item.name}
+          </h3>
+          <p className="">{item.ingredients.join(", ")}</p>
+          <p className="font-bold mt-4">${item.price}</p>
+        </div>
+        <button className="ml-auto border border-slate-50 rounded-md py-2 px-4 hover:bg-slate-50 hover:tracking-widest hover:font-bold">
+          Add
+        </button>
+        <div className="order-first flex items-center text-7xl">
           {item.image}
         </div>
       </div>
--- src/routes/Menu.tsx
+++ src/routes/Menu.tsx
@@ -2,9 +2,8 @@ import MenuCard from "../components/MenuCard"
 
 export default function Menu(): JSX.Element {
   return (
-    <main className="flex-1">
-      <h1>Menu page goes here</h1>
-      <div className="flex flex-col gap-8">
+    <main className="flex-1 flex justify-center">
+      <div className="sm:w-2/3 lg:w-1/2 flex flex-col">
         <MenuCard />
       </div>
     </main>
