24c953f56c031606dbc9a3cd095340645abbbda7
ajo <115482909+ajoborgvold@users.noreply.github.com>
2024-01-17 19:45:52 +0100

    Created shopping cart animation

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-import { useContext } from "react"
+import { useContext, useEffect, useState } from "react"
 import { Link, useLocation } from "react-router-dom"
 import { AppContext } from "../context/AppContext"
 import { FaShoppingCart } from "react-icons/fa"
@@ -6,8 +6,18 @@ import { FaArrowLeft } from "react-icons/fa6"
 
 export default function Header(): JSX.Element {
   const { cartCount } = useContext(AppContext)
+  const [isAnimated, setIsAnimated] = useState(false)
   const location = useLocation()
-  // console.log(location.pathname)
+
+  useEffect(() => {
+    setIsAnimated(true)
+    
+    const timeOut = setTimeout(() => {
+      setIsAnimated(false)
+    }, 1000)
+
+    return () => clearTimeout(timeOut)
+  }, [cartCount])
 
   return (
     <header className="flex items-center gap-2 bg-gradient-to-r from-amber-900 to-amber-500 text-slate-50 py-10 px-8 md:px-24">
@@ -22,13 +32,15 @@ export default function Header(): JSX.Element {
       {location.pathname === "/" ? (
         <Link to="/order" className="relative ml-auto group">
           <FaShoppingCart
-            className="text-amber-100 text-5xl md:text-6xl group-hover:text-amber-950 group-focus:text-amber-950"
+            className={`text-amber-100 text-5xl md:text-6xl group-hover:text-amber-950 group-focus:text-amber-950 ${isAnimated ? 'rotate-6' : ''}`}
             aria-label="Show shopping cart"
             aria-hidden="false"
           />
-          <p className="absolute top-1 right-4 md:top-2 md:right-5 flex justify-center items-center text-amber-950 group-hover:text-amber-100 group-focus:text-amber-100 text-lg md:text-xl tracking-wider">
-            {cartCount}
-          </p>
+          {cartCount !== 0 &&
+            <p className={`absolute top-1 right-4 md:top-2 md:right-5 flex justify-center items-center text-amber-950 group-hover:text-amber-100 group-focus:text-amber-100 text-lg md:text-xl tracking-wider ${isAnimated ? 'scale-150' : ''}`}>
+              {cartCount}
+            </p>
+          }
         </Link>
       ) : (
         <Link
