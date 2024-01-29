27bf232e405027a28eb007a791c3088c1a501c1b
ajo <115482909+ajoborgvold@users.noreply.github.com>
2024-01-29 17:38:58 +0100

    Responsive design fixes

--- src/components/OrderCard.tsx
+++ src/components/OrderCard.tsx
@@ -14,12 +14,16 @@ export default function OrderCard(): JSX.Element {
         key={item.name}
         className="grid grid-cols-3 gap-8 odd:bg-amber-200 dark:odd:bg-slate-700 even:bg-amber-100 dark:even:bg-slate-800 dark:text-slate-50 p-4 first:rounded-t last:rounded-b"
       >
-        <p className="font-semibold text-lg sm:text-xl md:text-2xl">{item.name}</p>
+        <p className="text-lg sm:text-xl md:text-2xl font-semibold">
+          {item.name}
+        </p>
         <div className="flex gap-4 sm:text-lg md:text-xl">
           <p>Quant:</p>
           <p>{itemQuantity}</p>
         </div>
-        <p className="ml-auto text-lg sm:text-xl md:text-2xl font-semibold">$ {itemPrice}</p>
+        <p className="ml-auto text-lg sm:text-xl md:text-2xl font-semibold">
+          $ {itemPrice}
+        </p>
       </li>
     )
   })
